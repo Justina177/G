5a099ed3d78b30a8147d90ef5ffaf4492d1cd5b4
justina177 <justinaenenche@yahoo.com>
2023-02-07 04:28:17 +0100

    initialize-an-ecommerce-app

---
+++ Ecommerce.b/frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <a href="/">ecommerce</a>
+      </header>
+      <main>
+        list product
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
