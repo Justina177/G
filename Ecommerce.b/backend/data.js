78876805fcdecf8dfa14105385136eb758aa471e
justina177 <justinaenenche@yahoo.com>
2023-02-10 16:07:14 +0100

    create Backend with Node.Js Server

---
+++ Ecommerce.b/backend/data.js
@@ -0,0 +1,56 @@
+const data = {
+    products: [
+        {
+            name: 'shirt 1 ',
+            slug: 'shirt1',
+            category: 'Mens Shirts',
+            image: '/images/shirt1.jpeg',
+            price: '150',
+            countInStock: '23',
+            brand: 'Gucci',
+            rating: '4.7',
+            numReviews: '17',
+            deescription: 'high quality shirt'
+        },
+
+        {
+            name: 'shirt 2',
+            slug: 'shirt2',
+            category: 'Men shirt',
+            image: '/images/shirt2.jpeg',
+            price: '130',
+            countInStock: '17',
+            brand: 'Nike',
+            rating: '4.6',
+            numReviews: '11',
+            deescription: 'high quality product'
+        },
+
+        {
+            name: 'Men suit',
+            slug: 'Mensuit1',
+            category: 'Suit',
+            image: '/images/suit1.jpg',
+            price: '350',
+            countInStock: '31',
+            brand: 'Italian',
+            rating: '4.8',
+            numReviews: '10',
+            deescription: 'Lastest brand of suit'
+        },
+
+        {
+            name: 'jacket',
+            slug: 'jacket1',
+            category: 'Jacket',
+            image: '/images/jacket1.jpg',
+            price: '390',
+            countInStock: '40',
+            brand: 'Puma',
+            rating: '4.5',
+            numReviews: '13',
+            deescription: 'Extra Thick Jacket'
+        },
+    ]
+}
+export default data
--- README.md
+++ README.md
@@ -11,4 +11,14 @@
 4. Added Page Routing
     i. created route for home screen   
     ii. created route for product screen  
-    installed react-router-dom
+    iii. installed react-router-dom
+5. Created Server for Backend
+   (Node.JS Server)
+   i. Created Backend folder and added     server.js file
+   ii. ran npm init in root folder
+   iii. updated package.json /set type:module
+   iv. Added start command as node backend/server.js
+   v. npm install express
+   vi. create route to return backend
+   vii. move products.js from frontend to backend
+   viii. create route for /api/products at a local host address of 5000
